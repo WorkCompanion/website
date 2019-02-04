bef396290eafd5564b028c93cbe95e6621440f8e
WorkCompanion <47313069+WorkCompanion@users.noreply.github.com>
2019-02-04 18:07:46 +0530

    Added files via upload
    all website src files uploaded

---
+++ Responsive---Start-to-finish/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready( function(){
+
+
+//hamburger menu toggle
+
+	$('.nav-toggle').click(function(){
+		$('.main-nav').toggleClass('is-open')
+		$('.hamburger').toggleClass('is-open')
+	})
+
+})
